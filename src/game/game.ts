713ee498361d2c177006fa8e69d93367939f8dcf
idovelemon <1322600812@qq.com>
2017-11-28 23:17:57 +0800

    细化流程
    1.添加TITLE流程
    2.添加Gameing流程

--- src/game/game.ts
+++ src/game/game.ts
@@ -3,15 +3,22 @@ enum GAME_STATE {
     GAMEING,
 }
 
+enum SUB_GAME_STATE {
+    ENTER,
+    RUN,
+    EXIT,
+}
+
 interface GameInfo {
     GameState: GAME_STATE;
+    SubGameState: SUB_GAME_STATE;
 }
     
 class Game {
     private gameInfo: GameInfo;
 
     constructor() {
-        this.gameInfo = {GameState: GAME_STATE.TITLE};
+        this.gameInfo = {GameState: GAME_STATE.TITLE, SubGameState: SUB_GAME_STATE.ENTER};
     }
 
     MainLoop(): void {
@@ -20,7 +27,7 @@ class Game {
     }
 
     Main(): void {
-        console.log("Main");
+        this.UpdateGameState();
     }
 
     Draw(): void {
@@ -28,12 +35,44 @@ class Game {
     }
 
     UpdateGameState(): void {
-        switch (this.gameInfo.GameState)
-        {
+        switch (this.gameInfo.GameState) {
             case GAME_STATE.TITLE:
+            this.UpdateTitle();
             break;
+
             case GAME_STATE.GAMEING:
+            this.UpdateGameing();
             break;
         }
+    }
+
+    UpdateTitle(): void {
+        switch (this.gameInfo.SubGameState) {
+            case SUB_GAME_STATE.ENTER:
+            this.UpdateTitleEnter();
+            break;
+
+            case SUB_GAME_STATE.RUN:
+            this.UpdateTitleRun();
+            break;
+
+            case SUB_GAME_STATE.EXIT:
+            this.UpdateTitleExit();
+            break;
+        }
+    }
+
+    UpdateTitleEnter(): void {
+    }
+
+    UpdateTitleRun(): void {
+    }
+
+    UpdateTitleExit(): void {
+
+    }
+
+    UpdateGameing(): void {
+
     }
 }
