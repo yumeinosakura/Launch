e026f5cd3efab787d53289ee413201058325526e
idovelemon <1322600812@qq.com>
2017-12-03 19:25:00 +0800

    实现map功能

--- src/util/map.ts
+++ src/util/map.ts
@@ -1,12 +1,62 @@
 module util {
-    class Map<T> {
-        private _hashMethod: (hash: number) => number;
-        private _container: T[];
+    // 默认hash函数
+    export function DefaultHashMethod<T>(hash: number, map: Map<T>): number {
+        let size = map.length();
+        return hash % size;
+    }
 
-        constructor(defaultSize: number, hashMethod: (hash: number) => number) {
+    interface Node<T> {
+        key: number;
+        value: T;
+    };
+
+    export class Map<T> {
+        private _hashMethod: (hash: number, map: Map<T>) => number;
+        private _container: List<Node<T>>[];
+
+        constructor(defaultSize: number, hashMethod: (hash: number, map: Map<T>) => number) {
             this._hashMethod = hashMethod;
             this._container = [];
             this._container.length = defaultSize;
+        }
+
+        insert(key: number, value: T): void {
+            let hashKey = this._hashMethod(key, this);
+
+            // 创建列表
+            if (this._container[hashKey] == null) {
+                this._container[hashKey] = new List<Node<T>>();
+            }
+            let container = this._container[hashKey];
+
+            // 检查是否有相同元素存在
+            for (let node of container) {
+                if (node.value == value) {
+                    return;
+                }
+            }
+
+            // 没有相同元素，插入列表头部
+            container.push_front({key: key, value: value});
+        }
+
+        find(key: number): T {
+            let hashKey = this._hashMethod(key, this);
+            let container: List<Node<T>> = this._container[hashKey];
+            
+            if (container != null) {
+                for (let node of container) {
+                    if (node.key == key) {
+                        return node.value;
+                    }
+                }
+            }
+
+            return undefined;
+        }
+
+        length(): number {
+            return this._container.length;
         }
     };
 };
